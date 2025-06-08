f409edd1aa0c9cd05bc6f6287db186491180c60c
MaksimNikiforov <145759459+fblfca@users.noreply.github.com>
2025-06-08 11:48:25 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+import pickle
+
+HOST = '127.0.0.1'
+PORT = 8080
+
+sock = socket.socket()
+sock.bind((HOST, PORT))
+sock.listen(1)
+
+conn, addr = sock.accept()Add commentMore actions
+
+msg = conn.recv(1024)
+print(pickle.loads(msg))
+
+conn.close()
